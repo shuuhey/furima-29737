ad24794a65fdb98c41bd0a530cce79c4755a7575
shuuhey <shuuhey.am.pm@gmail.com>
2020-09-22 20:36:58 +0900

    新規登録ページ修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true,
+  inclusion: { in: %w(@) }
+  # パスワード2回入力はdeviseの基本バリデーション
+  validates :encrypted_password, presence: true,
+  length: { minimum: 6 },
+  format: { with: /\A[0-9a-zA-Z]/ }
+  validates :family_name, presence: true,
+  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true,
+  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, presence: true,
+  format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true,
+  format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_day, presence: true
+
 end
