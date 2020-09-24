bd46df24894e290b7fe8f4112ab851fd791f2629
shuuhey <shuuhey.am.pm@gmail.com>
2020-09-24 20:08:32 +0900

    モデルの単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: true },
-  inclusion: { in: %w(@) }
+  validates :email, presence: true, uniqueness: { case_sensitive: true }, format: { with: /@/ }
   # パスワード2回入力はdeviseの基本バリデーション
   validates :password, presence: true,
-  length: { minimum: 6 },
-  format: { with: /\A[0-9a-zA-Z]/ }
+                       length: { minimum: 6 },
+                       format: { with: /\A[0-9a-zA-Z]/ }
   validates :family_name, presence: true,
-  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+                          format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name, presence: true,
-  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+                         format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :family_name_kana, presence: true,
-  format: { with: /\A[ァ-ヶー－]+\z/ }
+                               format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true,
-  format: { with: /\A[ァ-ヶー－]+\z/ }
+                              format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_day, presence: true
-
 end
