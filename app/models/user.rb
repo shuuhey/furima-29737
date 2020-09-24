3560046f12044425fc6a0e3e362bdedcbc5404b6
shuuhey <shuuhey.am.pm@gmail.com>
2020-09-24 14:01:46 +0900

    ログイン画面修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: true },
   inclusion: { in: %w(@) }
   # パスワード2回入力はdeviseの基本バリデーション
-  validates :encrypted_password, presence: true,
+  validates :password, presence: true,
   length: { minimum: 6 },
   format: { with: /\A[0-9a-zA-Z]/ }
   validates :family_name, presence: true,
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'comments/index'
   devise_for :users
+  root 'comments#index'
+  get 'comments/index'
 end
